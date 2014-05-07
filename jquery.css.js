5900b663ed282ae2908b838595e4a2acdd1bf41d
Joseph James Weitzel <joe@box.biz>
2014-05-07 01:56:39 -0400

    Update jquery.css.js

--- jquery.css.js
+++ jquery.css.js
@@ -1,6 +1,6 @@
 /**
  * css.js
- * jquery.css.js v0.1.1
+ * jquery.css.js v0.1.3
  * Copyright 2014 BOX Creative, LLC
  * Code licensed under MIT:
  * https://github.com/twbs/bootstrap/blob/master/LICENSE
@@ -17,10 +17,6 @@
  * 1) on() listeners
  * 2) Pass all rules to CSS.JS on -events- as well as .selectors.
  *    This could be great to access via a set var like $css or $rules.
- * 3) Handle attaching events more than once. AKA $.hasBinding()
- * 4) Create the ability to pass arguments to defined CSS.JS -functions-.
- *    EX: -sayANumber- { content: "alert('How about the number '+args[0])"; }
- *        .nav a { content: "!click(sayANumber:7)"; }
  * 5) Plugins
  * 6) Pseudo Class defaults. Ex: a:hover { content:"$this.css('opacity', 0.5)"; }
  *
@@ -29,42 +25,12 @@
  * each(key, event): $this, key, event
  * [jQueryEvent](event): $this, event
  * 
- *
- * Basic Useage:
- * -------
- * -on-document-ready {
- *     content: "console.log('Ready, woo hoo!')";
- * }
- *
- * document {
- *     content: "ready(on-document-ready)";
- * }
- *
- * #my-element {
- *     content: "console.log($this)";
- * }
- *
- * -remove-active-nav- {
- *     content: "$('.nav a.active').removeClass('active')";
- * }
- *
- * -on-nav-click- {
- *     content: "$this.addClass('active')";
- * }
- *
- * .nav a {
- *     content: click(remove-active-nav, on-nav-click);
- * }
- * 
- *
  * Known Issues:
  * -------------
  * - Safari: content:"" attribute on image (.svg in this case) cause it to be invisible
  * - Safari: Makes all selectors lowercase inside teh Rules JS Object CSSStyleRule.selectorText.
  *    We are checking a match after it returns UND, then attempt to lookup via
  *    CSSStyleRule.selectorText.toLowerCase() transformation to match.
- *
- *
  *
  **/
 
@@ -76,7 +42,7 @@
 	 * CssJs()
 	 * - CssJs Constructor.
 	 */
-	function CssJs() {
+	function CssJs(settingsKey, settingsValue) {
 		
 		/**
 		 * PUBLIC PROPERTIES
@@ -97,13 +63,34 @@
 		/**
 		 * Locally-global variables.
 		 */
-		var Dict = { selectors:{}, evals:{}, customEachCount:0 };
-		var tokens = {
-			'timeout({': 'setTimeout(func(){',
-			'.scroll({': '.scroll(func(){',
-			'.each({': '.scroll(func(){',
-			'func(': 'function('
-		};
+		var UND = 'undefined',
+				Dict = {
+					settings: {
+						functionsFirst: false,
+						funcPrefix: '-',
+						funcSuffix: '-'
+					},
+					selectors:{},
+					evals:{},
+					customEachCount:0
+				},
+				prefix = Dict.settings.funcPrefix,
+				suffix = Dict.settings.funcSuffix,
+				tokens = {
+					'Data.': 'CSSJS.data.',
+					'timeout({': 'setTimeout(func(){',
+					'.scroll({': '.scroll(func(){',
+					'.each({': '.scroll(func(){',
+					'func(': 'function('
+				},
+				variablesCode = 'var self = this, CSSJS = self, $document = $(document), $window = $(window); '
+					+'for(var _eval in Dict.evals) if(_eval.toLowerCase().indexOf(\''+prefix+'vars\')===0) '
+					+'eval(syntax(Dict.evals[_eval], true, true));';
+		
+		/**
+		 * Handle init settings
+		 */
+		_settings(settingsKey, settingsValue);
 		
 		/**
 		 * PUBLIC METHODS
@@ -128,6 +115,45 @@
 		};
 		
 		/**
+		 * .settings(mixed key[, mixed val])
+		 *  - Can be called again. Good for reparsing AJAX content. !untested 
+		 */
+		// Private method
+		function _settings(key, val) {
+			// Add tokens in object
+			if(typeof key === 'object' && typeof val === UND) {
+				Dict.settings = $.extend(Dict.settings, key);
+				return;
+			// Make sure we have both values
+			} else if(typeof key === UND || typeof val === UND) {
+				return;
+			};
+			// Force to string if not
+			if(typeof key !== 'string')
+				key = key.toString();
+			// Add to tokens
+			Dict.settings[key] = val;
+		};
+		// Public method
+		this.settings = function(key, val){
+			_settings(key, val);
+		};
+		
+		/**
+		 * .dispatch(str funcName[, $ $this])
+		 * - Dispatches an event
+		 */
+		this.dispatch = function(funcName, $this){
+			// Add all variables to be used in CSS in this scope
+			eval(variablesCode);
+			// Dispatch the event
+			var code = Dict.evals[prefix+funcName+suffix];
+			if(typeof code == UND)
+				code = Dict.evals[prefix+funcName.toLowerCase()+suffix];
+			eval(code);
+		};
+		
+		/**
 		 * .addToken(mixed find[, str replace])
 		 *  - Can be called again. Good for reparsing AJAX content. !untested 
 		 */
@@ -146,7 +172,7 @@
 			if(typeof replace !== 'string')
 				replace = replace.toString();
 			// Add to tokens
-			tokens[find] =replace;
+			tokens[find] = replace;
 		};
 		
 		/**
@@ -154,15 +180,8 @@
 		 * Binds all CSS.JS rules as jQuery events
 		 */
 		this.bind = function() {
-			// Add some variables to be used in CSS in this scope
-			var self = this, CSSJS = self, returned;
-			var $document = $(document), $window = $(window);
-			// Get CSS.JS variables -VARS NAMESPACE- { content:"..." }
-			for(var _eval in Dict.evals) {
-				if(_eval.toLowerCase().indexOf('-vars')===0){
-					eval(syntax(Dict.evals[_eval], true, true));
-				};
-			};
+			// Add all variables to be used in CSS in this scope
+			eval(variablesCode);
 			// Loop through the selectors with proper content attributes
 			$.each(Dict.selectors, function(selector, events){
 				// Get our jQuery DOM selector
@@ -183,28 +202,29 @@
 						// Bind all methods other than jQuery.each
 						if(fn.name != 'each') {
 							$selector.data('CssJs', {preventDefault:fn.preventDefault});
+							$selector.unbind(eventBinding);
 							$selector.bind(eventBinding, function(event){
 								var $this = $(this);
 								var data = $this.data('CssJs');
 								if(data.preventDefault) event.preventDefault();
-								var code = Dict.evals['-'+funcName+'-'];
-								if(typeof code === 'undefined')
-									code = Dict.evals['-'+funcName.toLowerCase()+'-'];
+								var code = Dict.evals[prefix+funcName+suffix];
+								if(typeof code === UND)
+									code = Dict.evals[prefix+funcName.toLowerCase()+suffix];
 								code = 'var response = (function(event){'+code+'}(event));';
 								code = syntax(code);
 								eval(code);
-								if(typeof response !== 'undefined') returned = response;
+								if(typeof response !== UND) returned = response;
 								if(data.preventDefault) return false;
 							});
 						// Evaluate each each method right away
 						} else if(fn.name == 'each') {
 							$selector.each(function(key, event){
 								var $this = $(this);
-								var code = Dict.evals['-'+funcName+'-'];
+								var code = Dict.evals[prefix+funcName+suffix];
 								// Safari makes the selector lowercase for some reason.
 								// In case, well see if it matches in lowercase
-								if(typeof code == 'undefined')
-									code = Dict.evals['-'+funcName.toLowerCase()+'-'];
+								if(typeof code == UND)
+									code = Dict.evals[prefix+funcName.toLowerCase()+suffix];
 								eval(code);
 							});
 						};
@@ -216,7 +236,7 @@
 				};
 			});
 			// Set firstRun so document ready only happends once
-			self.firstRun = true;
+			self.firstRun = false;
 		};		
 		
 		/**
@@ -242,11 +262,13 @@
 						// Get it's selector and make sure its not a before or after psedudo class
 						var selectorText = rule.selectorText;
 						if(selectorText.search(/\:(before|after)$/) > -1) return true;
-						// Check the type of rule this is...
-						var event = selectorText.search(/^\-.*\-$/g) > -1;
+						// Check syntax and parse tokens
+						content = syntax(content, false, false);
 						content = parseTokens(content);
+						// Check the type of rule this is...
+						var evals = (selectorText.indexOf(prefix)===0 && selectorText.split(suffix).pop()=='');
 						// Save the rule.
-						Dict[event?'evals':'selectors'][selectorText] = content;
+						Dict[evals?'evals':'selectors'][selectorText] = content;
 					}
 				});
 			});
@@ -257,10 +279,15 @@
 		 * Syntax completion and fixes
 		 */
 		function syntax(code, openWithVar, closeWidthColon) {
-			openWithVar = typeof openWithVar == 'undefined' ? false : openWithVar;
-			closeWidthColon = typeof closeWidthColon == 'undefined' ? false : closeWidthColon;
+			openWithVar = typeof openWithVar == UND ? false : openWithVar;
+			closeWidthColon = typeof closeWidthColon == UND ? false : closeWidthColon;
 			// Trim
 			code = code.replace(/^\s*|\s*$/g, '');
+			// Hanging commas
+			code = code.replace(/\,\s*\)/g, ')');
+			code = code.replace(/\,\s*\}/g, '}');
+			// Comments /*...*/
+			code = code.replace(/\/\*.*\*\/$/g, '');
 			// Code completions
 			if(openWithVar && code.search(/^var\s{1,}/g) === -1) code = 'var '+code;
 			if(closeWidthColon && code.search(/\;$/g) === -1) code += ';';
@@ -285,7 +312,7 @@
 				var evalEvent = 'INLINE-EACH-EVENT-'+Dict.customEachCount;
 				Dict.evals['-'+evalEvent+'-'] = code;
 				Dict.customEachCount++;
-				refFuncs.push('each('+evalEvent+')');
+				refFuncs[Dict.settings.functionsFirst?'push':'unshift']('each('+evalEvent+')');
 			};
 			// Generate fn objects array
 			var array = [];
@@ -312,8 +339,26 @@
 		 * Helper function to parse code with tokens allowing smaller code in css
 		 */
 		function parseTokens(content){
+			// We can call other CSS functions from content code, like this:
+			// CSSJS.dispatch('funcName', $this);
+			// But its a lot to write, so lets parse to that from this format:
+			// -funcName($this)
+			var dispatches = content.match(/[\w\-]{1,}\([\w\$]*\)/g);
+			if(dispatches != null) dispatches = dispatches.filter(function(e){return e.indexOf(prefix)===0;});
+			if(dispatches != null && dispatches.length > 0) {
+				for(var _d in dispatches) {
+					var disp = dispatches[_d];
+					var funcName = disp.substring(disp.lastIndexOf(prefix)+1, disp.lastIndexOf('('));
+					var arg = disp.substring(disp.lastIndexOf('(')+1, disp.lastIndexOf(')'));
+					var replace = 'CSSJS.dispatch(\''+funcName+'\'';
+					replace += (arg.length ? (', '+arg+')') : ')');
+					content = content.split(disp).join(replace);
+				}
+			};
+			// Find and replace all simple tokens
 			for(var find in tokens)
 				content = content.split(find).join(tokens[find]);
+			// Return tokenized content code
 			return content;
 		};
 		
@@ -327,8 +372,7 @@
 	 * on $document.ready()
 	 */
 	$(function(){
-		var CSSJS = new CssJs();
-		CSSJS.init();
+		window.CSSJS = new CssJs().init();
 	});
 
 /**
